b37550b5865392bf579b8142afbe81b37c182b9b
spragala <spragala@yahoo.com>
2017-07-23 23:43:53 -0700

    wip - isAunthenticated not a function

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 // Get Homepage
-router.get('/', function (req, res) {
+router.get('/', checkAuthentication, function (req, res) {
   res.render('index');
 });
+
+function checkAuthentication(res, req, next) {
+  console.log(req);
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    req.flash('error_msg', 'You are not logged in');
+    res.redirect('users/login');
+  }
+}
 
 module.exports = router;
