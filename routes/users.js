d250b78bffd1450bcf81630527b0c4e3fd7d0bb1
spragala <spragala@yahoo.com>
2017-07-23 22:32:35 -0700

    Login function added, updated login form to accept username

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/user');
 
 // Signup
@@ -42,13 +44,55 @@ router.post('/signup', function (req, res) {
 
     User.createUser(newUser, function (err, user) {
       if (err) throw err;
-      console.log(user)
     });
 
     req.flash('success_msg', 'You have successfully created an account, please login.');
 
-    res.redirect('/users/login')
+    res.redirect('/users/login');
   }
 }); // <- router.post
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    User.getUserByUsername(username, function (err, user) {
+      if (err) throw err;
+      if (!user) {
+        return done(null, false, {
+          message: 'Unknown User'
+        });
+      }
+
+      User.comparePassword(password, user.password, function (err, isMatch) {
+        if (err) throw err;
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, {
+            message: 'Invalid password'
+          });
+        }
+      });
+    });
+  }));
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.getUserById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+router.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  }),
+  function(req, res) {
+    res.redirect('/');
+  });
 
 module.exports = router;
