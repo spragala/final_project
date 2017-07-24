70a0a4e6c24c6576d83c37fae574b95276952ae5
spragala <spragala@yahoo.com>
2017-07-24 13:39:15 -0700

    Fixed isAuthenticated bug

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 // Get Homepage
-router.get('/', checkAuthentication, function (req, res) {
+router.get('/', checkAuth, function (req, res) {
   res.render('index');
 });
 
-function checkAuthentication(res, req, next) {
-  console.log(req);
+function checkAuth(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   } else {
     req.flash('error_msg', 'You are not logged in');
-    res.redirect('users/login');
+    res.redirect('/users/login');
   }
 }
 
